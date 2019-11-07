a62f88b96c37700f40d3c6387eb4a77421457f0b
Tim <timrogers32@gmail.com>
2019-11-06 16:46:26 -0800

    Starting off strong :)

--- AngularRPG/public/src/app/app.module.ts
+++ AngularRPG/public/src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpService } from './http.service'; //////import service here
+import { HttpClientModule } from '@angular/common/http'; ////allows http requests
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,9 +13,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [HttpService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
